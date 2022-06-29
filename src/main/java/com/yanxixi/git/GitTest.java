7a0e0e692419d0a28e28452e19388d47b84bf9c3
Yanxixi <yanxixi@nju.com>
2022-06-29 16:05:21 +0800

    first commit

---
+++ src/main/java/com/yanxixi/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.yanxixi.git;
+
+/**
+ * @Author YanXiXi
+ * @ClassName GitTest
+ * @Description git 测试
+ * @Date 16:01 2022/6/29
+ * @Version 1.0
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
