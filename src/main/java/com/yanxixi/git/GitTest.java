91f3e5f11b8a73858845f8b6661b2d22f3207c8d
Yanxixi <yanxixi@nju.com>
2022-06-29 17:06:08 +0800

    push first commit

--- src/main/java/com/yanxixi/git/GitTest.java
+++ src/main/java/com/yanxixi/git/GitTest.java
@@ -15,5 +15,6 @@ public class GitTest {
         System.out.println("hello git hot-fix1");
         System.out.println("hello git hot-fix2");
         System.out.println("hello git master1");
+        System.out.println("hello push1");
     }
 }
