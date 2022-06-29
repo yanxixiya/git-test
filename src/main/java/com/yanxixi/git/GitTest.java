7310ac1d3cd8aa0bcd29e07921ba7e9a2b364cba
Yanxixi <yanxixi@nju.com>
2022-06-29 16:09:57 +0800

    second commit

--- src/main/java/com/yanxixi/git/GitTest.java
+++ src/main/java/com/yanxixi/git/GitTest.java
@@ -10,5 +10,7 @@ package com.yanxixi.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git2");
     }
 }
